2a28b4e2d02fe215b5829078f7ae2bc1a374d82e
Jan Decker <Jan.Decker23@gmail.com>
2024-09-13 15:51:44 +0200

    the 3d models update

---
+++ src/components/HomeInfo.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { arrow } from '../assets/icons'
+
+
+const InfoBox = ({text, link, btnText}) => (
+    <div className='info-box'>
+       <p className='font-medium sm:text-xl text-center'>{text}</p>
+        <Link to={link} className="neo-brutalism-white neo-btn">
+            {btnText}
+            <img src={arrow} className='w-4 h-4 object-contain' />
+        </Link>
+    </div>
+)
+
+
+const renderContent = {
+    1: (
+        <h1 className='sm:text-xl sm:leading-snug text-center  neo-brutalism-blue bg-black py-4 px-8 text-white mx-5 opacity-90'>
+            Hi, i am <span className='font-semibold'>Jan Decker</span>👋
+            <br />
+            I am a frontend developer from Czech Republic
+        </h1>
+    ),
+    2: (
+        <InfoBox
+            text="Worked with many companies and picked up many skills along the way."
+            link="/about"
+            btnText="About me"
+        />
+    ),
+    3: (
+        <InfoBox
+            text="Led multiple projects to success over the years"
+            link="/projects"
+            btnText="My portfolio"
+        />
+    ),
+    4: (
+        <InfoBox
+            text="Need a project done or looking for a dev? I'm just a few keystrokes away"
+            link="/contact"
+            btnText="Contact me"
+        />
+    ),
+}
+
+const HomeInfo = ({ currentStage }) => {
+  return renderContent[currentStage] || null
+}
+
+export default HomeInfo
--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -13,21 +13,29 @@ const Bird = () => {
         actions['Take 001'].play()
     }, [])
 
-    useFrame((clock, camera)=>{
-        birdRef.current.position.y = 0.5 * Math.sin(clock.elapsedTime) * 0.2 + 2
+    useFrame(({clock, camera})=>{
+        birdRef.current.position.y =  Math.sin(clock.elapsedTime) * 0.2 + 2
+
+        if(birdRef.current.position.x > camera.position.x + 10) {
+            birdRef.current.rotation.y = Math.PI
+        } else if(birdRef.current.position.x < camera.position.x - 10) {
+            birdRef.current.rotation.y = 0
+        }
+        
 
         if (birdRef.current.rotation.y === 0) {
-            birdRef.current.rotation.x += 0.01
-            birdRef.current.rotation.z -= 0.01
+            birdRef.current.position.x += 0.03
+            birdRef.current.position.z -= 0.03
         } else {
-            birdRef.current.rotation.x -= 0.01
-            birdRef.current.rotation.z += 0.01
+            birdRef.current.position.x -= 0.03
+            birdRef.current.position.z += 0.03
         }
     })
   return (
     <mesh 
-    position={[5, 0, 0]} 
-    rotation={[0, 0, 0]} scale={[1, 1, 1]} 
+    position={[-5, 2, 3]} 
+    rotation={[0, 0, 0]} 
+    scale={[0.003, 0.003, 0.003]} 
     ref={birdRef}>
      <primitive object={scene} />  
     </mesh>
